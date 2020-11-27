27abe6b0b0e7da136bd4e32790459af586e32f4a
Vladimir Mukhin <vlad@mukhin.page>
2020-11-27 13:47:28 -0500

    Switch to requests

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ from python:latest
 
 COPY main.py .
 
+RUN python3 -m pip install requests
+
 CMD python3 main.py
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import http.server
 import socketserver
-from urllib import request
+import requests
 from os import getenv
 import json
 
@@ -16,6 +16,27 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
 
         print(self.headers)
+
+        try:
+            headers = {
+                'x-request-id'     : self.headers['x-request-id'],
+                'x-b3-traceid'     : self.headers['x-b3-traceid'],
+                'x-b3-spanid'      : self.headers['x-b3-spanid'],
+                'x-b3-parentspanid': self.headers['x-b3-parentspanid'],
+                'x-b3-sampled'     : self.headers['x-b3-sampled'],
+                'x-b3-flags'       : self.headers['x-b3-flags'],
+                'b3'               : self.headers['b3']
+            }
+            result = requests.get(coordinates_url, headers=headers)
+            data = json.loads(result.text)
+
+        except Exception as e:
+            print(e)
+            data = {'altitude': 'None', 'latitude': 'None'}
+
+
+        
+
 
         self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
         self.wfile.write(bytes(f"<body><p>altitude: {data['altitude']} latitude: {data['latitude']}</p>", "utf-8"))
